0102faca494a47b3d27da4420bdc3b75f6644825
Cory Carson <gogocorycarson@yandex.com>
2021-12-01 00:06:26 -0800

    Basic script ported

--- README.md
+++ README.md
@@ -2,14 +2,11 @@
 
 The PWN fan is not rotating when X715 is connected to PI 4, we need to to install the pwm fan control script
 
-The pytone file need pigpiod library, so we need to install it firstly.
+Tested on Ubuntu 21.10 arm64
 
-```
-sudo apt-get install -y pigpio python-pigpio python3-pigpio
-sudo systemctl enable pigpiod
-git clone https://github.com/geekworm-com/x715
-sudo reboot
+https://ubuntu.com/tutorials/gpio-on-raspberry-pi#1-overview
 
-cd ~
-python /home/pi/x715/pwm_fan_control.py&
+```bash
+sudo apt-get install -y python3-lgpio
+sudo python3 pwm_fan_control.py
 ```
--- pwm_fan_control.py
+++ pwm_fan_control.py
@@ -1,42 +1,66 @@
-#!/usr/bin/python
-import RPi.GPIO as GPIO
-import pigpio
-import time
+#!/usr/bin/env python3
+# Min version 3.8
 
-try:
-     servo = 13
+import asyncio
+from typing import Final
+from contextlib import asynccontextmanager
+import lgpio
 
-     pwm = pigpio.pi()
-     pwm.set_mode(servo, pigpio.OUTPUT)
-     pwm.set_PWM_frequency( servo, 25000 )
-     pwm.set_PWM_range(servo, 100)
-     while(1):
-          #get CPU temp
-          file = open("/sys/class/thermal/thermal_zone0/temp")
-          temp = float(file.read()) / 1000.00
+@asynccontextmanager
+async def _gpio_servo():
+     h = lgpio.gpiochip_open(0)
+     if h >= 0:
+          try:
+               yield h
+          finally:
+               lgpio.gpiochip_close(h)
+     else:
+          raise RuntimeError("GPIO handle not acquired...")
+
+async def _get_temp() -> float:
+     """Get CPU temp"""
+     SENSOR: Final[str] = "/sys/class/thermal/thermal_zone0/temp"
+
+     with open(SENSOR, mode="r") as f:
+          temp = float(f.read()) / 1000.00
           temp = float('%.2f' % temp)
-          file.close()
+          return temp
 
-          if(temp > 30):
-               pwm.set_PWM_dutycycle(servo, 40)
+async def main():
+     SERVO: Final[int] = 13
+     FREQUENCY: Final[int] = 2500 #hz
+     DEFAULT_DUTY_CYCLE: Final[int] = 100 # 100%
 
-          if(temp > 50):
-               pwm.set_PWM_dutycycle(servo, 50)
+     async with _gpio_servo() as h:
+          lgpio.gpio_claim_output(h, SERVO)
+          lgpio.tx_pwm(h, SERVO, FREQUENCY, DEFAULT_DUTY_CYCLE)
+          duty_cycle = DEFAULT_DUTY_CYCLE
 
-          if(temp > 55):
-               pwm.set_PWM_dutycycle(servo, 75)
+          while True:
+               temp: float = await _get_temp()
 
-          if(temp > 60):
-               pwm.set_PWM_dutycycle(servo, 90)
+               assert temp is not None
 
-          if(temp > 65):
-               pwm.set_PWM_dutycycle(servo, 100)
+               if temp < 30:
+                    duty_cycle = 0
+               elif 50 > temp >= 30:
+                    duty_cycle = 40
+               elif 55 > temp >= 50:
+                    duty_cycle = 50
+               elif 60 > temp >= 55:
+                    duty_cycle = 75
+               elif 65 > temp >= 60:
+                    duty_cycle = 90
+               else:
+                    duty_cycle = 100
 
-          if(temp < 30):
-               pwm.set_PWM_dutycycle(servo, 0)
-          time.sleep(1)
+               assert 100 >= duty_cycle >= 0
 
+               lgpio.tx_pwm(h, SERVO, FREQUENCY, duty_cycle)
 
-except KeyboardInterrupt:
-    GPIO.cleanup() 
+               print(f"Temp: {temp} --> {duty_cycle}%")
 
+               await asyncio.sleep(3)
+
+if __name__ == "__main__":
+     asyncio.run(main())
