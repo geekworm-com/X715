c49ed1de2063e070e5cc3ba2ccfd36624e011aa9
Cory Carson <gogocorycarson@yandex.com>
2021-12-02 00:02:36 -0800

    prettify

--- pwm_fan_control.py
+++ pwm_fan_control.py
@@ -1,46 +1,93 @@
 #!/usr/bin/env python3
 # Min version 3.8
+"""Raspberry Pi 4, Geekwork X715, modern Ubuntu fan control
+
+Requires python 3.8 or newer, which is included with Ubuntu 20.04 LTS and newer.
+Requires lpgio:
+     `apt install python3-lgpio`
+
+Examples:
+     python3 -OO pwm_fan_control.py
+     python3 -OO pwm_fan_control.py --gpio 13
+     python3 -OO pwm_fan_control.py --gpiochip 0 --gpio 13
+"""
 
 import asyncio
-from contextlib import contextmanager
-from typing import Any, Final
+import argparse
+import contextlib
+import pathlib
+import signal
+from typing import Any, Final, Optional
 
 import lgpio
 
 
-@contextmanager
-def _gpio_servo() -> Any:
-     handle = lgpio.gpiochip_open(0)
-     if handle >= 0:
-          try:
-               yield handle
-          finally:
-               lgpio.gpiochip_close(handle)
-     else:
-          raise RuntimeError("GPIO handle not acquired...")
+@contextlib.contextmanager
+def _gpiochip(gpiochip: int) -> Any:
+     """Context manager for lgpio.gpiochip
+     
+     Example:
+          import lgpio
+          with _gpiochip(0) as handle:
+               with _gpio_claim_output(handle, 12):
+                    lpgio.gpio_write(handle, 12, 1)
+     """
+     handle: Final[Any] = lgpio.gpiochip_open(gpiochip)
+     # Nullable logic, compare against the positive case and invert if needed
+     if not handle >= 0:
+          raise lgpio.error(f"GPIO handle not acquired: {handle}")
 
-@contextmanager
+     try:
+          yield handle
+     finally:
+          error_code = lgpio.gpiochip_close(handle)
+          # Nullable logic, compare against the positive case and invert if needed
+          if not error_code >= 0:
+               raise lgpio.error(f"GPIO handle not released: {handle} {error_code}")
+
+
+@contextlib.contextmanager
 def _gpio_claim_output(handle: Any, gpio: int) -> int:
+     """Context manager for lgpio gpio output
+     
+     Example:
+          import lgpio
+          with _gpiochip(0) as handle:
+               with _gpio_claim_output(handle, 12):
+                    lpgio.gpio_write(handle, 12, 1)
+     """
      error_code = lgpio.gpio_claim_output(handle, gpio)
-     if error_code >= 0:
-          try:
-               yield gpio
-          finally:
-               lgpio.gpio_free(handle, gpio)
-     else:
-          raise RuntimeError(f"GPIO output not acquired: {error_code}")
+     # Nullable logic, compare against the positive case and invert if needed
+     if not error_code >= 0:
+          raise lgpio.error(f"GPIO output not claimed: {gpio} {error_code}")
+
+     try:
+          yield gpio
+     finally:
+          error_code = lgpio.gpio_free(handle, gpio)
+          # Nullable logic, compare against the positive case and invert if needed
+          if not error_code >= 0:
+               raise lgpio.error(f"GPIO output not freed: {gpio} {error_code}")
 
 async def _get_temp() -> float:
      """Get CPU temp"""
-     SENSOR: Final[str] = "/sys/class/thermal/thermal_zone0/temp"
+     SENSOR_FILE: Final[pathlib.Path] = pathlib.Path("/sys/class/thermal/thermal_zone0/temp")
 
-     with open(SENSOR, mode="r") as f:
-          temp = float(f.read()) / 1000.00
+     with open(SENSOR_FILE, mode="r") as f:
+          raw: str = f.read()
+          temp = float(raw) / 1000.00
           temp = float('%.2f' % temp)
+          assert temp is not None
           return temp
 
 def _temp_to_duty_cycle(temp: float) -> int:
+     """Fan curve
+     
+     Original curve provided by the vendor.
+     """
      assert temp is not None
+
+     duty_cycle: int
 
      if temp < 30:
           duty_cycle = 0
@@ -59,30 +106,76 @@ def _temp_to_duty_cycle(temp: float) -> int:
 
      return duty_cycle
 
-async def main():
-     SERVO: Final[int] = 13
+async def fan_control(gpiochip: int, gpio: int):
+     """Task for controlling the fan based on temp. Runs until cancelled."""
      FREQUENCY: Final[int] = 10000 #hz
-     DEFAULT_DUTY_CYCLE: Final[int] = 100 # 100%
-     duty_cycle = DEFAULT_DUTY_CYCLE
 
-     with _gpio_servo() as h:
-          with _gpio_claim_output(h, SERVO) as _:
+     with _gpiochip(gpiochip) as h:
+          with _gpio_claim_output(h, gpio):
                try:
-                    lgpio.tx_pwm(h, SERVO, FREQUENCY, DEFAULT_DUTY_CYCLE)
                     while True:
                          temp = await _get_temp()
 
+                         assert temp is not None
+
                          duty_cycle = _temp_to_duty_cycle(temp)
 
-                         lgpio.tx_pwm(h, SERVO, FREQUENCY, duty_cycle)
+                         assert 100 >= duty_cycle >= 0
+
+                         queue_length = lgpio.tx_pwm(h, gpio, FREQUENCY, duty_cycle)
+                         # Nullable logic, compare against the positive case and invert if needed
+                         if not queue_length >= 0:
+                              raise lgpio.error(f"GPIO unable to set PWM: {queue_length} {duty_cycle}")
 
                          print(f"Temp: {temp} --> {duty_cycle}%")
 
                          await asyncio.sleep(3)
 
-               finally:
-                    print("Cleanup...")
-                    lgpio.tx_pwm(h, SERVO, 0, 0)
+               except asyncio.CancelledError:
+                    print("Cancelling...")
+                    duty_cycle = 0
+                    queue_length = lgpio.tx_pwm(h, gpio, FREQUENCY, duty_cycle)
+                    # Nullable logic, compare against the positive case and invert if needed
+                    if not queue_length >= 0:
+                         raise lgpio.error(f"GPIO unable to set PWM: {queue_length} {duty_cycle}")
+
+                    raise
+
+async def main():
+     """Main entry point for PWM fan control"""
+     parser = argparse.ArgumentParser(add_help=True)
+
+     DEFAULT_GPIOCHIP: Final[int] = 0
+     parser.add_argument(
+          '--gpiochip',
+          type=int,
+          default=DEFAULT_GPIOCHIP,
+          help=("GPIO chip to use. "
+                f"The Rpi4 hat is {DEFAULT_GPIOCHIP}, which is selected by default.")
+     )
+
+     DEFAULT_GPIO: Final[int] = 13
+     parser.add_argument(
+          '--gpio', 
+          type=int, 
+          default=DEFAULT_GPIO,
+          help=("GPIO mask to control the fan with. "
+                f"Geekwork X715 uses mask {DEFAULT_GPIO}, which is selected by default."))
+
+     args: Final[argparse.Namespace] = parser.parse_args()
+     
+     task: Final[asyncio.Task] = asyncio.create_task(fan_control(args.gpiochip, args.gpio), name="fan control")
+
+     loop = asyncio.get_running_loop()
+     for sig in [signal.SIGINT, signal.SIGHUP, signal.SIGTERM]:
+          loop.add_signal_handler(sig, lambda sig=sig: task.cancel(sig))
+     del loop, sig
+
+     try:
+          await task
+     except asyncio.CancelledError:
+          print("Cancelled")
+
 
 if __name__ == "__main__":
      asyncio.run(main())
